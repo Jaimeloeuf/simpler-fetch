118418e5ca57a5b1e7aa2d0ff83e66930793c1de
Jaime <jaimeloeuf@gmail.com>
2022-03-31 19:51:02 +0800

    Refactor out code to make lib modular.

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-/**
- * Simple fetch abstraction to refactor the API and does body stringification if needed
- * @param {String} url
- * @param {object} opts
- * @param {object | String | undefined} body
- */
-export const _fetch = (url, opts = {}, body) =>
-  fetch(url, {
-    ...opts,
-
-    // Only include the body field if a body value is provided
-    // Stringify the body object if it isn't already
-    body: body && typeof body === "object" ? JSON.stringify(body) : body,
-  });
+import { _fetch } from "./src/_fetch";
 
 /**
  * fcf: Functional, Curried, Fetch abstraction over `_fetch`
