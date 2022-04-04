8c4042ff3e4345a98c5ecbf2fa3ed21cc005ff54
Jaime <jaimeloeuf@gmail.com>
2022-04-04 16:51:49 +0800

    Update test server to return on all routes.

--- test/server/index.js
+++ test/server/index.js
@@ -3,15 +3,10 @@ const app = express();
 
 app.use(require("cors")({ origin: "*" }));
 
-app.get("/test", (req, res) => res.status(200).send({ test: "working" }));
-
-app.post("/test", express.json(), (req, res) =>
-  res.status(200).send({
-    auth: req.headers.authorization,
-    headers: req.headers,
-    received: req.body,
-  })
-);
+app.all("*", express.json(), function (req, res) {
+  const { url, method, headers, body } = req;
+  res.status(200).json({ ok: true, url, method, headers, body });
+});
 
 const port = process.env.PORT || 3000; // Defaults to PORT 3000
 app.listen(port, () => console.log(`Server running on port: ${port}`));
