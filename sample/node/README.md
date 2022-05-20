0fe9ef90db24ee4081ef9a85c5b5b98e8e9d9dec
Jaime <jaimeloeuf@gmail.com>
2022-05-21 00:09:34 +0800

    Create sample node script:     Create sample node js project to demo the use of this library in node.

---
+++ sample/node/README.md
@@ -0,0 +1,16 @@
+# node
+- Sample project using library in `node.js` with node-fetch as fetch polyfill.
+- The script demonstrates the use of `oof` in particular as that is **the recommended way** to use this library.
+- This script is a es6 module, as this library and its fetch polyfill can only be used as a es6 module.
+
+
+## Running the script
+1. Make sure the [API server](../server/) is already up and running
+1. Install dependencies
+    ```shell
+    npm install
+    ```
+1. Run the script
+    ```shell
+    npm run serve
+    ```
---
+++ sample/node/index.js
@@ -0,0 +1,81 @@
+// These only support es6 module import
+import fetch from "node-fetch";
+import { oof } from "../../dist/index.js";
+
+// This checks if fetch API is already available globally,
+// and only polyfilling it if it is not available with node-fetch library.
+//
+// Once you upgrade to node v18.0.0 and above, fetch API will be available in node natively,
+// meaning that you do not need to install any third party fetch library before using this!
+if (!global.fetch) {
+  console.log("Setting fetch globally\n");
+  global.fetch = fetch;
+}
+
+// async IIFE to use async await without using top level await as older node versions dont support it
+(async function () {
+  // Set base URL of your API, leave out the trailing '/'
+  // if you plan to use a starting '/' for every API call.
+  oof._baseUrl = "http://localhost:3000";
+  console.log("oof._baseUrl: ", oof._baseUrl);
+
+  /* ================================= GET ================================= */
+
+  // API call to registered API server
+  await oof
+    .GET("/test")
+    .runJSON()
+    .then((res) => console.log("res 0", res));
+
+  // API call to registered API server with header set in multiple ways
+  await oof
+    .GET("/test")
+    // Fixed header object
+    .header({ headerOne: 1 })
+    // Synchronous function that returns a header object
+    .header(() => ({ headerTwo: 1 }))
+    // Asynchronous function that returns a promise that resolves to a header object
+    .header(async () => ({ headerThree: 3 }))
+    .runJSON()
+    .then((res) => console.log("res 1", res));
+
+  // API call to local API server with full path
+  await oof
+    .GET("http://localhost:3000/test")
+    .runJSON()
+    .then((res) => console.log("res 2", res));
+
+  // API call to external API server with full path
+  await oof
+    .GET("https://jsonplaceholder.typicode.com/todos/1")
+    .runJSON()
+    .then((res) => console.log("res 3", res));
+
+  /* ================================= POST ================================= */
+
+  // POST request to registered API server
+  await oof
+    .POST("/test")
+    .data({ some: "data" })
+    .runJSON()
+    .then((res) => console.log("res 4", res));
+
+  // POST request to local API server with full path
+  await oof
+    .POST("http://localhost:3000/test")
+    .data({ some: "data" })
+    .runJSON()
+    .then((res) => console.log("res 5", res));
+
+  /* ================================= Error Handling ================================= */
+
+  console.log("\n\nNext API call will fail to show case error handling.\n");
+
+  // API call to a definitely not available site to similiar API call failed
+  await oof
+    .GET("https://hopefully-this-not-registered.com/some/invalid/path")
+    .runJSON()
+    .then((res) => console.log("res", res))
+    // Catch error and handle here to prevent the error from bubbling up
+    .catch((err) => console.error("API call failed\n", err));
+})();
