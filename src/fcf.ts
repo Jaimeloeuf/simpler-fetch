778affe62c0f046c4c0c0e8944795a918a2c3a48
Jaime <jaimeloeuf@gmail.com>
2022-06-14 15:17:31 +0800

    Support async options generating function in fcf.

--- src/fcf.ts
+++ src/fcf.ts
@@ -3,13 +3,13 @@ import { _fetch } from "./_fetch.js";
 /**
  * fcf: Functional, Curried, Fetch abstraction over `_fetch`
  *
- * Allow user to pass a function for the option field to generate options/headers right before
+ * Allow user to pass a function (can be async) for the option field to generate options/headers right before
  * the fetch call instead of generating first, to allow partially applied options that needs
  * to be generated just before the call like a time limited auth token or recaptcha token.
  */
 export const fcf =
   (baseUrl: string) =>
   (path: string) =>
-  (opts: RequestInit | Function = {}) =>
-  (body?: object | string) =>
-    _fetch(baseUrl + path, typeof opts === "function" ? opts() : opts, body);
+  (opt: RequestInit | Function = {}) =>
+  async (body?: object | string) =>
+    _fetch(baseUrl + path, typeof opt === "function" ? await opt() : opt, body);
