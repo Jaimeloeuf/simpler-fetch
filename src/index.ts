1cce7b7ab7a25c37b7fcbc14cdf3bb75ab84b98a
Jaime <jaimeloeuf@gmail.com>
2022-08-27 23:52:46 +0800

    Add docs for HTTP methods and constructor:     Add docs detailing the only times where a library user should be     using the constructor method directly is when they would like to use     one of the HTTP methods (HEAD/OPTIONS) that do not have a static     constructor wrapper method defined.

--- src/index.ts
+++ src/index.ts
@@ -23,20 +23,32 @@ export type Header =
   | (() => Promise<Record<string, any> | undefined>);
 
 /**
- * All the supported HTTP methods
+ * All the supported HTTP methods.
+ * Ref: https://fetch.spec.whatwg.org/#methods
+ *
+ * Based on the reference link, although there are other HTTP methods, they are not
+ * supported by the `fetch` spec, and therefore not included in this union type as
+ * even if you could pass it to the `fetch` function, it will just fail.
+ *
+ * `HEAD` and `OPTIONS` HTTP methods are quite special and low level and extremely
+ * rarely used, therefore these are the only 2 methods which do not have static
+ * constructor wrapper methods to easily create `oof` instances for these methods.
+ * Because of that, the only times where a library user will use the constructor
+ * directly is when they want to use one of these 2 HTTP methods.
  *
  * Exporting this type so that you can explicitly type your HTTP method strings
  * as needed to ensure that they are correctly typed at point of value definition
  * instead of only type checking when you call the constructor.
  */
 export type HTTPMethod =
-  | "HEAD"
-  | "OPTIONS"
   | "GET"
   | "POST"
   | "PUT"
   | "PATCH"
-  | "DELETE";
+  | "DELETE"
+  // For the methods below, see JSDoc
+  | "HEAD"
+  | "OPTIONS";
 
 /**
  * JsonResponse type is used as the base type for what can be returned from the server when using `runJSON()`.
@@ -204,7 +216,17 @@ export class oof {
    */
   #body?: any;
 
-  /** Low level constructor API that generally isnt used. Stick with the provided static methods for a cleaner API. */
+  /**
+   * Low level constructor API that generally isnt used.
+   *
+   * For most library users, just stick with the provided static methods for a cleaner API.
+   *
+   * `HEAD` and `OPTIONS` HTTP methods are quite special and low level and extremely
+   * rarely used, therefore these are the only 2 methods which do not have static
+   * constructor wrapper methods to easily create `oof` instances for these methods.
+   * Because of that, the only times where a library user will use the constructor
+   * directly is when they want to use one of these 2 HTTP methods.
+   */
   constructor({
     method,
     path,
