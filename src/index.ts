7d8a858a5e22cb2ef9aa9aafac316dfff715c1d6
Jaime <jaimeloeuf@gmail.com>
2023-05-26 01:06:03 +0800

    Simplify entry barrel file export:     Simplify the export pattern used in the entry barrel file now that     TS 5.0 and above supports exporting types with wildcard.

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,7 @@ export { oof } from "./oof";
 export * from "./errors/index";
 export * from "./utils/index";
 
-// Export type only works with named exports
-export type {
-  HTTPMethod,
-  Header,
-  HeaderValue,
-  RequestError,
-  ApiResponse,
-  Validator,
-} from "./types";
+// Use type modifier to ensure only types are exported. This only works with
+// TS > 5.0, else it would export all as values instead of marking these as type
+// only exports.
+export type * from "./types";
