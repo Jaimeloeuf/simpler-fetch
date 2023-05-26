a9c2843bcd7d9d510e4e07b379d95496fc7bc7ab
Jaime <jaimeloeuf@gmail.com>
2023-05-26 23:49:51 +0800

    Update RequestError type:     Following the new custom errors, RequestError is updated to include     those, and to replace the generic Error class type with the     HeaderError class type since it was meant to represent all     unspecified errors thrown from Header generation functions.

--- src/types/RequestError.ts
+++ src/types/RequestError.ts
@@ -1,18 +1,36 @@
+import type { TimeoutError, HeaderError, ValidationError } from "../errors";
+
 /**
- * Unifying all possible errors that can be thrown by fetch into a single error
- * type, covering possible errors thrown by header function(s) too with the
- * generic `Error` class.
+ * Union type for all possible errors that can be **returned** from an API call.
+ * This covers all possible errors from network failure to errors thrown from
+ * header function(s), to runtime response validation failure.
  *
- * See this reference on all errors that can be thrown:
+ * Basically this is the type of the error that can be returned from the API
+ * call safely.
+ * ```typescript
+ * const { res, err } = ... --> API call
+ *
+ * err; --> This `err` is `RequestError` type
+ * ```
+ *
+ * This does not include the `oofError` type since that will be **thrown** and
+ * not returned when the oof library is not correctly configured / used.
+ *
+ * See this reference on all errors that can be thrown by the `fetch` API itself
  * https://developer.mozilla.org/en-US/docs/Web/API/fetch#exceptions
- *
- * RequestError is exported from the library so that users can inspect this
- * union type.
  */
 export type RequestError =
-  // AbortError
+  // AbortError caused by custom timeouts
   | DOMException
-  // If the network fails
+
+  // If the network / API call itself fails
   | TypeError
-  // Generic Error type for when the header function throws....?
-  | Error;
+
+  // Custom error returned when API call exceeds custom timeout limit.
+  | TimeoutError
+
+  // Custom error returned when a Header function throws an error.
+  | HeaderError
+
+  // Custom error returned when response fails runtime response validation.
+  | ValidationError;
