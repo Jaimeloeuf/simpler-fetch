ae1c2f06352f1672f1e8544240d799d13daec87e
Jaime <jaimeloeuf@gmail.com>
2023-05-27 21:59:33 +0800

    Update RequestError docs:     Update RequestError docs to explain that not all possible Error     types were part of the union type since user defined error types     thrown during runtime response validation cant possibly be part of     the union, but users can still check for it in a type safe manner     using the instanceof operator.

--- src/types/RequestError.ts
+++ src/types/RequestError.ts
@@ -1,9 +1,10 @@
 import type { TimeoutError, HeaderError, ValidationError } from "../errors";
 
 /**
- * Union type for all possible errors that can be **returned** from an API call.
- * This covers all possible errors from network failure to errors thrown from
- * header function(s), to runtime response validation failure.
+ * Union type for all 'simpler-fetch' defined errors that can be **returned**
+ * from an API call. This covers all possible errors from network failure to
+ * errors thrown from header function(s), to runtime response validation failure
+ * where these are standard expected errors.
  *
  * Basically this is the type of the error that can be returned from the API
  * call safely.
@@ -13,8 +14,19 @@ import type { TimeoutError, HeaderError, ValidationError } from "../errors";
  * err; --> This `err` is `RequestError` type
  * ```
  *
- * This does not include the `oofError` type since that will be **thrown** and
- * not returned when the oof library is not correctly configured / used.
+ * ## Errors not included
+ * However there are some error types that are not part of this union,
+ * specifically `oofError` and library user defined errors.
+ *
+ * 1. This does not include the `oofError` type since that will be **thrown**
+ * and not returned when the oof library is not correctly configured / used.
+ *
+ * 2. When doing runtime response validation, if the validator throws a custom
+ * error type, e.g. user uses 'zod' and it throws a `ZodError`, this union type
+ * will obviously not include that. However despite that, library users can
+ * still check for that error type using `err instanceof ZodError` in a type
+ * safe manner, since instanceof operator can be used for narrowing even when
+ * it is not defined on the union type itself.
  *
  * See this reference on all errors that can be thrown by the `fetch` API itself
  * https://developer.mozilla.org/en-US/docs/Web/API/fetch#exceptions
