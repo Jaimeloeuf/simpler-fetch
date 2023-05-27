54f5b0532b0f2e13492bd7ae0853b8964adb528e
Jaime <jaimeloeuf@gmail.com>
2023-05-28 00:02:21 +0800

    Add utility to print groups easily in sample.

--- sample/webapp-ts/src/demo/2 basics.ts
+++ sample/webapp-ts/src/demo/2 basics.ts
@@ -1,5 +1,6 @@
 import { oof } from "simpler-fetch";
 import { baseIdentifier } from "./0 Base Identifiers.js";
+import { Print } from "../utils.js";
 
 /**
  * All sample API calls are nested in block scopes to reuse variable names
@@ -10,7 +11,7 @@ export async function basics() {
   {
     const { res, err } = await oof.useDefault().GET("/test").runJSON();
 
-    console.log("res 1", res, err);
+    Print.group("API call with the default base Url", res, err);
   }
 
   // API call with a non default base Url
@@ -20,7 +21,7 @@ export async function basics() {
       .GET("/test")
       .runJSON();
 
-    console.log("res 2", res, err);
+    Print.group("API call with a non default base Url", res, err);
   }
 
   // Make a one off API call to an external API service with a full URL path without using any base Urls
@@ -30,6 +31,10 @@ export async function basics() {
       .GET()
       .runJSON();
 
-    console.log("res 3", res, err);
+    Print.group(
+      "One off call to an external API whose base URL is not saved",
+      res,
+      err
+    );
   }
 }
--- sample/webapp-ts/src/utils.ts
+++ sample/webapp-ts/src/utils.ts
@@ -6,3 +6,35 @@ export async function runAsSection(f: Function, sectionName: string) {
   await f();
   console.groupEnd();
 }
+
+/**
+ * Singleton object used to manage the console log printing process.
+ */
+export const Print = {
+  // Aliases for the console methods to make access uniform through `Print`
+  log: console.log,
+  err: console.error,
+
+  /**
+   * Start a new nested group before printing
+   */
+  start(groupName: string) {
+    console.groupCollapsed(groupName);
+  },
+
+  /**
+   * End the last created nested group
+   */
+  end() {
+    console.groupEnd();
+  },
+
+  /**
+   * Print something as a nested group
+   */
+  group(groupName: string, ...optionalParams: any[]) {
+    console.groupCollapsed(groupName);
+    console.log(...optionalParams);
+    console.groupEnd();
+  },
+};
