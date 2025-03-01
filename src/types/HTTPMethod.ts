73dc5b6ff637e359ff725b82e9feaf095302ba24
Jaime <jaimeloeuf@gmail.com>
2025-03-01 18:25:36 +0900

    Simplify HTTPMethod type:     Can do so now that all HTTP methods have a unified access method.

--- src/types/HTTPMethod.ts
+++ src/types/HTTPMethod.ts
@@ -6,11 +6,6 @@
  * not supported by the `fetch` spec, and therefore not included in this union
  * type as even if you could pass it to the `fetch` function, it will just fail.
  *
- * `HEAD` and `OPTIONS` HTTP methods are quite special and low level and rarely
- * used, therefore these are the only 2 methods which do not have wrapper
- * methods to easily create `Fetch` instances from `Builder` instance. However
- * library users can simply use the low level `HTTP` method on `Builder`.
- *
  * Exporting this type so that you can explicitly type your HTTP method strings
  * as needed to ensure that they are correctly typed at point of value
  * definition instead of only type checking when you call the constructor.
@@ -21,6 +16,5 @@ export type HTTPMethod =
   | "PUT"
   | "PATCH"
   | "DELETE"
-  // For the methods below, see JSDoc
   | "HEAD"
   | "OPTIONS";
