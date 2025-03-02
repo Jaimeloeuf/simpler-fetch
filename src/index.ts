3ec7cfe3019d9bfdb0807ce76562aad382c7b434
Jaime <jaimeloeuf@gmail.com>
2025-03-02 17:26:28 +0900

    Clean up unnecessary docs.

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,4 @@ export { sf } from "./sf";
 export * from "./errors";
 export * from "./exceptions";
 export * from "./utils";
-
-// Use type modifier to ensure only types are exported. This only works with
-// TS > 5.0, else it would export all as values instead of marking these as type
-// only exports.
 export type * from "./types";
