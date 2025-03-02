e5a2a7288f5c08bb79e751b05bb823e99c00d4b1
Jaime <jaimeloeuf@gmail.com>
2025-03-02 17:25:40 +0900

    Use explicit type only export.

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,10 @@
  * Barrel file for all the types.
  */
 
-export * from "./Header";
-export * from "./HTTPMethod";
-export * from "./JsonTypeAlias";
-export * from "./RequestException";
-export * from "./ApiResponse";
-export * from "./Validator";
-export * from "./ResponseParser";
+export type * from "./Header";
+export type * from "./HTTPMethod";
+export type * from "./JsonTypeAlias";
+export type * from "./RequestException";
+export type * from "./ApiResponse";
+export type * from "./Validator";
+export type * from "./ResponseParser";
