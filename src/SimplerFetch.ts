72af9b08ab1f4f4ab8042028df8d85153b007c62
Jaime <jaimeloeuf@gmail.com>
2025-03-08 23:43:19 +0800

    Copy default base URL code from sf to SimplerFetch.

--- src/SimplerFetch.ts
+++ src/SimplerFetch.ts
@@ -1,4 +1,5 @@
 import type { BaseUrlConfig } from "./types/BaseUrlConfig";
+import { sfError } from "./errors";
 import { MethodBuilder } from "./MethodBuilder";
 
 export class SimplerFetch<
@@ -25,5 +26,32 @@ export class SimplerFetch<
   useBaseUrl(identifier: BaseUrlIdentifiers) {
     // @todo Explain why we can use non-null assertion here
     return this.#urlIdToMethodBuilder.get(identifier)!;
+  }
+
+  /**
+   * Private property used to track default base URL identifier set by user.
+   */
+  #defaultBaseUrlIdentifier?: BaseUrlIdentifiers;
+
+  /**
+   * Set a particular identifier and baseUrl mapping as the default mapping to
+   * use so that you can easily use this default mapping in your API calls.
+   *
+   * When called this overrides any previously set default baseUrl mapping, and
+   * this method should ideally only be ever called once in your whole app.
+   */
+  setDefaultBaseUrl(identifier: BaseUrlIdentifiers) {
+    this.#defaultBaseUrlIdentifier = identifier;
+  }
+
+  /**
+   * Use the default baseUrl set with `setDefaultBaseUrl`.
+   */
+  useDefaultBaseUrl() {
+    if (this.#defaultBaseUrlIdentifier === undefined) {
+      throw new sfError("sf: Default identifier not set");
+    }
+
+    return this.useBaseUrl(this.#defaultBaseUrlIdentifier);
   }
 }
