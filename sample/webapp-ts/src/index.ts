d53a256bb53caba0415226f30693d59c68dd728b
Jaime <jaimeloeuf@gmail.com>
2025-03-20 17:47:31 +0800

    Delete demo 12 since overrides are not possible:     The new API requires the default options and headers to be set at     the point of instantiation and cannot ever be overwritten so this     potential concern/actor vector does not exist anymore.

--- sample/webapp-ts/src/index.ts
+++ sample/webapp-ts/src/index.ts
@@ -9,7 +9,6 @@ import { customTimeout } from "./demo/7 Custom Timeout.js";
 import { customOptions } from "./demo/8 Custom Options.js";
 import { exceptionTypeNarrowing } from "./demo/10 Exception type narrowing.js";
 import { lazyLoading } from "./demo/11 Lazy Loading.js";
-import { overrideDefaultValues } from "./demo/12 Override default values.js";
 import { otherResponseDataTypes } from "./demo/13 Other response data types.js";
 import { queryParams } from "./demo/14 query params.js";
 
@@ -34,8 +33,6 @@ import { printGroup } from "./utils.js";
   await printGroup("Error Type Narrowing", exceptionTypeNarrowing);
 
   await printGroup("Lazy Loading", lazyLoading);
-
-  await printGroup("Safely override default values", overrideDefaultValues);
 
   await printGroup("Other Response data types", otherResponseDataTypes);
 
