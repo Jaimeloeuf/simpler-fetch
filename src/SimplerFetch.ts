3631a4a673017776362785c2844fc176ac13b082
Jaime <jaimeloeuf@gmail.com>
2025-03-31 04:50:54 +0800

    WIP Add HTTP Method methods to SimplerFetch:     So that the initial builder pattern threading point starts with the     HTTP method name.

--- src/SimplerFetch.ts
+++ src/SimplerFetch.ts
@@ -1,4 +1,5 @@
-import type { BaseUrlConfig } from "./types";
+import type { BaseUrlConfig, HTTPMethod } from "./types";
+import { UrlBuilder } from "./UrlBuilder";
 import { MethodBuilder } from "./MethodBuilder";
 
 /**
@@ -67,4 +68,36 @@ export class SimplerFetch<
       defaultOptions: {},
       defaultHeaders: [],
     });
+
+  #ChainToUrlBuilder = <const HTTPMethodUsed extends HTTPMethod>(
+    method: HTTPMethodUsed
+  ) =>
+    new UrlBuilder<BaseUrlIdentifiers, HTTPMethodUsed>(
+      this.#urlIdToMethodBuilder,
+
+      // Create a new object that will be the chainable fetch config object
+      // threaded through all the Builder class instances.
+      { method }
+    );
+
+  /** Make a `HEAD` API call */
+  HEAD = () => this.#ChainToUrlBuilder("HEAD");
+
+  /** Make a `OPTIONS` API call */
+  OPTIONS = () => this.#ChainToUrlBuilder("OPTIONS");
+
+  /** Make a `GET` API call */
+  GET = () => this.#ChainToUrlBuilder("GET");
+
+  /** Make a `POST` API call */
+  POST = () => this.#ChainToUrlBuilder("POST");
+
+  /** Make a `PUT` API call */
+  PUT = () => this.#ChainToUrlBuilder("PUT");
+
+  /** Make a `PATCH` API call */
+  PATCH = () => this.#ChainToUrlBuilder("PATCH");
+
+  /** Make a `DELETE` API call */
+  DEL = () => this.#ChainToUrlBuilder("DELETE");
 }
