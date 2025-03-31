6f16cea2c93cafad7c904648d3d6fde1fcf2eb6a
Jaime <jaimeloeuf@gmail.com>
2025-04-01 00:02:23 +0800

    Split UrlBuilder up:     So that at run time it will guide users by only providing the     options that are available, i.e. do not expose the useSavedBaseUrl     method if user did not instantiate SimplerFetch with any base url.

--- src/UrlBuilder.ts
+++ src/UrlBuilder.ts
@@ -1,53 +1,27 @@
 import type { HTTPMethod } from "./types";
-import type { ExpectedFetchConfig_for_UrlBuilder } from "./ChainableFetchConfig";
-import { MethodBuilder } from "./MethodBuilder";
+import type {
+  ExpectedFetchConfig_for_UrlBuilder,
+  ExpectedFetchConfig_for_PathBuilder,
+} from "./ChainableFetchConfig";
+import { PathAndQueryParamsBuilder } from "./PathAndQueryParamsBuilder";
 
 /**
- * Builder pattern class for users to use a base URL or a custom URL.
+ * Builder pattern class for users to use a custom URL.
  */
-export class UrlBuilder<
-  const BaseUrlIdentifiers,
-  const HTTPMethodUsed extends HTTPMethod
-> {
-  constructor(
-    /**
-     * Mapping of urlId (Base URL Identifiers) to `MethodBuilder`.
-     */
-    private readonly urlIdToMethodBuilder: Map<
-      BaseUrlIdentifiers,
-      MethodBuilder
-    >,
-
-    private readonly config: ExpectedFetchConfig_for_UrlBuilder
-  ) {}
+export class UrlBuilder<const HTTPMethodUsed extends HTTPMethod> {
+  constructor(private readonly config: ExpectedFetchConfig_for_UrlBuilder) {}
 
   /**
-   * Use a base URL that is saved during `SimplerFetch` instance creation.
-   */
-  useSavedBaseUrl = (identifier: BaseUrlIdentifiers) =>
-    // Non-null assertion operator can be safely used here as TS already
-    // typechecks to ensure that the identifier passed in is a valid generic
-    // `BaseUrlIdentifiers` string literal. This will only fail if user is not
-    // using TS which is a case that we are not covering.
-    this.urlIdToMethodBuilder.get(identifier)!;
-
-  /**
-   * Use a Full URL string (with API path) to make an API call without using any
-   * base URL.
+   * Use given url string as base URL to make an API call.
    *
    * This method should be used when making one off API calls without having to
    * configure a base URL and its options. Usually used when you need to make an
    * API call to another domain, e.g. integrating with third party APIs.
    */
-  useUrl = (fullUrlString: string) =>
-    // Set generic type `IsFullUrl` to true to ensure that users are not allowed
-    // to set `path` values, instead they have to set the full URL string here.
-    new MethodBuilder<true>({
-      url: fullUrlString,
-
-      // A single use API call will always have empty default options/headers
-      // since it cannot be used/reused again by definition.
-      defaultOptions: {},
-      defaultHeaders: [],
-    });
+  useUrl(url: string) {
+    this.config.url = url;
+    return new PathAndQueryParamsBuilder<HTTPMethodUsed>(
+      this.config as ExpectedFetchConfig_for_PathBuilder
+    );
+  }
 }
---
+++ src/UrlWithSavedBaseUrlBuilder.ts
@@ -0,0 +1,45 @@
+import type { HTTPMethod, BaseUrlConfig } from "./types";
+import type {
+  ExpectedFetchConfig_for_UrlBuilder,
+  ExpectedFetchConfig_for_PathBuilder,
+} from "./ChainableFetchConfig";
+import { PathAndQueryParamsBuilder } from "./PathAndQueryParamsBuilder";
+
+/**
+ * Builder pattern class for users to use a saved base URL or a custom URL.
+ */
+export class UrlWithSavedBaseUrlBuilder<
+  const BaseUrlIdentifiers extends string,
+  const HTTPMethodUsed extends HTTPMethod
+> {
+  constructor(
+    private readonly config: ExpectedFetchConfig_for_UrlBuilder,
+    private readonly baseUrlConfigs: Record<BaseUrlIdentifiers, BaseUrlConfig>
+  ) {}
+
+  /**
+   * Use a base URL that is saved during `SimplerFetch` instance creation.
+   */
+  useSavedBaseUrl(identifier: BaseUrlIdentifiers) {
+    this.config.url = this.baseUrlConfigs[identifier].url;
+    this.config.defaultOptions = this.baseUrlConfigs[identifier].defaultOptions;
+    this.config.defaultHeaders = this.baseUrlConfigs[identifier].defaultHeaders;
+    return new PathAndQueryParamsBuilder<HTTPMethodUsed>(
+      this.config as ExpectedFetchConfig_for_PathBuilder
+    );
+  }
+
+  /**
+   * Use given url string as base URL to make an API call.
+   *
+   * This method should be used when making one off API calls without having to
+   * configure a base URL and its options. Usually used when you need to make an
+   * API call to another domain, e.g. integrating with third party APIs.
+   */
+  useUrl(url: string) {
+    this.config.url = url;
+    return new PathAndQueryParamsBuilder<HTTPMethodUsed>(
+      this.config as ExpectedFetchConfig_for_PathBuilder
+    );
+  }
+}
